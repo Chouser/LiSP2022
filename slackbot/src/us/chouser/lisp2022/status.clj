344e37c41d52481c19f07b60e92fd1d6c012559c
Chouser <chouser@n01se.net>
2022-05-01 22:59:50 -0400

    Snapshot -- posting unicode histogram working

--- slackbot/src/us/chouser/lisp2022/status.clj
+++ slackbot/src/us/chouser/lisp2022/status.clj
@@ -1,8 +1,9 @@
 (ns us.chouser.lisp2022.status
   (:require [clojure.java.io :as io]
-            [clj-http.lite.client :as http]
+            [clj-http.client]
             [clojure.data.json :as json]
-            [oz.core :as oz]))
+            [oz.core :as oz]
+            [clojure.string :as str]))
 
 (defn slack [xoxb method & [query-params]]
   (let [req {:url (str "https://slack.com/api/" (name method))
@@ -10,7 +11,7 @@
              :accept :json
              :headers {"Authorization" (str "Bearer " xoxb)}
              :query-params query-params}
-        resp (http/request req)
+        resp (clj-http.client/request req)
         body-data (json/read-str (:body resp) :key-fn keyword)]
     (when (-> body-data :response_metadata :next_cursor seq)
       (println "WARN: next_cursor was returned"))
@@ -33,18 +34,6 @@
                                {k (-> profile :fields v :value)})
                              {:next-chapter-start :Xf03EBCRLYCQ
                               :last-section-done  :Xf03DML42MGB}))))))))
-
-(defn go []
-  (let [{:keys [xoxb]} (read-string (slurp "secrets.edn"))]
-    (def x (all-user-profiles xoxb))
-
-    #_
-    (slack xoxb :chat.postMessage
-           {:channel "U03BTEC3352"
-            :blocks (json/write-str [{"type" "image",
-                                      "block_id" "image4",
-                                      "image_url" "",
-                                      "alt_text" "An incredibly cute kitten."}])})))
 
 (def col04 " ⡀⡄⡆⡇")
 (def col08 " ⡀⣀⣄⣤⣦⣶⣷⣿")
@@ -83,12 +72,6 @@
 ⣰  ⣱  ⣲  ⣳  ⣴  ⣵  ⣶  ⣷  ⣸  ⣹  ⣺  ⣻  ⣼  ⣽  ⣾  ⣿
 ")
 
-(def sample
-  (map (fn [_]
-         (let [x (- (* 2 (rand)) 1)]
-           (int (* x x x 30))))
-       (range 40)))
-
 (defn clipstr
   "Return a string of exactly length `len` by either truncating or adding spaces
   to the end of `string`."
@@ -97,27 +80,35 @@
     (str string (apply str (repeat (- len (count string)) " ")))
     (subs string 0 len)))
 
-(defn histogram [xs]
+(def yyyy-MM-dd (java.text.SimpleDateFormat. "yyyy-MM-dd"))
+(def MM-dd (java.text.SimpleDateFormat. "MM-dd"))
+
+(defn histogram [xs minrange labelfn]
   (let [chars col08
         xscale 40
         yscale 8
         maxchar (dec (count chars))
         minx (apply min xs)
         maxx (apply max xs)
-        xfactor (/ xscale (inc (- maxx minx)))
+        [minx maxx] (if (< minrange (- maxx minx))
+                      [minx maxx]
+                      (let [minx (/ (+ minx maxx (- minrange)) 2)]
+                        [minx (+ minx minrange)]))
+        xfactor (/ xscale (- maxx minx))
         freqs (frequencies (map #(Math/round (float (* xfactor (- % minx)))) xs))
         maxy (apply max (vals freqs))
         yfactor (/ yscale (inc maxy))
-        peakx (some (fn [[k v]] (when (= v maxy) k)) freqs)
-        labels [[0 (str "^ " minx)]
-                [peakx (str "^ " (float (+ minx (/ peakx xfactor))))]
-                [(dec xscale) (str "^ " maxx)]]]
+        peakcol (some (fn [[k v]] (when (= v maxy) k)) freqs)
+        peakx (+ minx (/ peakcol xfactor))
+        labels [[0 (str "^ " (labelfn minx))]
+                [(dec peakcol) (str " ^ " (labelfn peakx))]
+                [xscale (str "^ " (labelfn maxx))]]]
     (concat
      (for [row (range (/ yscale maxchar) 0 -1)]
        (apply str
-              (for [x (range xscale)]
+              (for [x (range (inc xscale))]
                 (get chars
-                     (let [y (int (+ 0.9 (* yfactor (freqs x 0))))]
+                     (let [y (int (+ 0.9999 (* yfactor (freqs x 0))))]
                        (cond
                          (< y (* (dec row) maxchar)) 0
                          (>= y (* row maxchar)) maxchar
@@ -127,28 +118,48 @@
               ""
               labels)])))
 
+(defn go1 []
+  (let [{:keys [xoxb]} (read-string (slurp "secrets.edn"))]
+    (def x (all-user-profiles xoxb))
+
+    #_
+    (slack xoxb :chat.postMessage
+           {:channel "U03BTEC3352"
+            :blocks (json/write-str [{"type" "image",
+                                      "block_id" "image4",
+                                      "image_url" "",
+                                      "alt_text" "An incredibly cute kitten."}])})))
+
+(defn go2 []
+  (let [{:keys [xoxb]} (read-string (slurp "secrets.edn"))
+        xs (concat
+            (keep :next-chapter-start x)
+            (repeatedly 30 #(format "2022-07-%02d" (rand-int 30))))
+        lines (histogram (map #(.getTime (.parse yyyy-MM-dd %)) xs)
+                         (* 40 1000 60 60 24)
+                         #(.format MM-dd %))]
+    (slack xoxb :chat.postMessage
+           {:channel "U03BTEC3352"
+            :blocks (json/write-str
+                     [{:type :header
+                       :text {:type :plain_text
+                              :text "Progress report aggregation"}}
+                      {:type :section
+                       :text {:type :mrkdwn
+                              :text (str/join
+                                     "\n"
+                                     (concat ["When should we start Chapter 2?"
+                                              "```"]
+                                             lines
+                                             ["```"]))}}])})))
+
 (comment
+
   (map #(select-keys % [:display_name :next-chapter-start :last-section-done]) (filter :next-chapter-start x))
 
-  (oz/start-plot-server!)
-  (oz/v!
-   {:data {:values (map #(-> %
-                             (select-keys [:next-chapter-start])
-                             (assoc :y (rand-int 10)))
-                    (concat
-                     x
-                     (for [x (range 100)]
-                       {:next-chapter-start (format "2022-%02d-%02d"
-                                                    (+ 9 (rand-int 3))
-                                                    (inc (rand-int 29)))})))}
-    :mark "circle"
-    :encoding {:x {:field :next-chapter-start :type :temporal
-                   :axis {:grid false, :title "When to start chapter 4"}}
-               :y {:field :y :type :quantitative
-                   :axis nil}}
-    :config {:style {:cell {:stroke :transparent}}}
-    :width 500
-    :height 20})
-
+  (let [sample (repeatedly 40 #(format "2022-05-%02d" (rand-int 10)))]
+    (run! println (histogram (map #(.getTime (.parse yyyy-MM-dd %)) sample)
+                             (* 40 1000 60 60 24)
+                             #(.format MM-dd %))))
 
   :end)
